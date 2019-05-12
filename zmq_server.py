fc259e808383645448d0566ccb000abbb7702ec6
VickeeX <727951723@qq.com>
2019-05-12 17:51:52 +0800

    reconstruct zmq_server

--- zmq_server.py
+++ zmq_server.py
@@ -9,7 +9,8 @@
 
 """A Socket subclass that adds some serialization methods."""
 
-import zlib, zmq, pickle
+import zlib, zmq, pickle, time
+import numpy as np
 
 
 class SerializingSocket(zmq.Socket):
@@ -55,20 +56,29 @@ class SerializingSocket(zmq.Socket):
 class SerializingContext(zmq.Context):
     _socket_class = SerializingSocket
 
-# def main():
-#     shared_states = np.zeros(shape=(5, 32, 84, 84, 4), dtype=np.uint8)
-#     shared_actions = np.zeros(shape=(5, 32, 6), dtype=np.float32)
-#     shared_rewards = np.zeros(shape=(5, 32,), dtype=np.float32)
-#     data = []
-#     for e, (s, a, r) in enumerate(zip(shared_states, shared_actions, shared_rewards)):
-#         data.append((e, s, a, r))
-#
-#     ctx = SerializingContext()
-#     req = ctx.socket(zmq.REQ)
-#     req.connect("tcp://127.0.0.1:6666")
-#
-#     # print("Array is %i bytes" % (A.nbytes))
-#
-#     req.send_zipped_pickle(data)
-#     req.close()
-#
+
+def main():
+    # shared_states = np.zeros(shape=(5, 32, 84, 84, 4), dtype=np.uint8)
+    # shared_actions = np.zeros(shape=(5, 32, 6), dtype=np.float32)
+    # shared_rewards = np.zeros(shape=(5, 32,), dtype=np.float32)
+    # data = []
+    # for e, (s, a, r) in enumerate(zip(shared_states, shared_actions, shared_rewards)):
+    #     data.append((e, s, a, r))
+
+    ctx = SerializingContext()
+    rep = ctx.socket(zmq.REP)
+    rep.bind("tcp://127.0.0.1:6666")
+
+    while True:
+        data = rep.recv_zipped_pickle()
+        print("Checking zipped pickle...")
+        # print("Okay" if (shared_rewards[2] == B[2][3]).all() else "Failed")
+        rep.send_string("received data.")
+        print("ok.")
+
+        # rep.close()
+        # rep.send_array(A, copy=False)
+
+
+if __name__ == '__main__':
+    main()
